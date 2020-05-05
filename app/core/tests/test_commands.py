7a2354f4e6719e255e564e3e1c7891cd58d349d1
Oguzhan San <sanoguzhan@hotmail.com>
2020-05-05 21:53:45 +0200

    [fix] files are grouped

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,12 +1,9 @@
 from unittest.mock import patch
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
-
 class CommandTests(TestCase):
-
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
