7863be8e6da4a978086768d1c7f67272a09eccfa
Oguzhan San <sanoguzhan@hotmail.com>
2020-05-05 22:44:14 +0200

    [fix] PEP8 refactor

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,6 +3,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
+
 class CommandTests(TestCase):
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
@@ -15,6 +16,6 @@ class CommandTests(TestCase):
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.side_effect = [OperationalError] * 5 + [True]
+            gi.side_effect = [OperationalError] * 5 + [True, ]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
