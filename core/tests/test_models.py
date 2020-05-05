98554d4854ce5b3ef571e8e5e144ebab795c18ff
Oguzhan San <sanoguzhan@hotmail.com>
2020-05-05 11:17:32 +0200

    [feat] unit testing added for user models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -15,3 +15,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@TesTasTTT.COM"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="21312FooBARf"
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "TestTest")
